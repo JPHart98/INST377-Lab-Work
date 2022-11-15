c4a1d7e4931e3afb0062c55551507558e88ff87b
JPHart98 <89784428+JPHart98@users.noreply.github.com>
2022-11-15 11:23:47 -0500

    Fixed double quotes in .js file

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -17,15 +17,15 @@ function getRandomIntInclusive(min, max) {
 }
 
 function injectHTML(list) {
-  console.log("fired injectHTML");
-  const target = document.querySelector("#restaurant_list");
-  target.innerHTML = "";
+  console.log('fired injectHTML');
+  const target = document.querySelector('#restaurant_list');
+  target.innerHTML = '';
 
-  const listEl = document.createElement("ol");
+  const listEl = document.createElement('ol');
   target.appendChild(listEl);
 
   list.forEach((item) => {
-    const el = document.createElement("li");
+    const el = document.createElement('li');
     el.innerText = item.name;
     listEl.appendChild(el);
   });
@@ -45,7 +45,7 @@ function injectHTML(list) {
 }
 
 function processRestaurants(list) {
-  console.log("fired restaurants list");
+  console.log('fired restaurants list');
   const range = [...Array(15).keys()];
   const newArray = range.map((item) => {
     const index = getRandomIntInclusive(0, list.length);
@@ -88,17 +88,17 @@ async function mainEvent() {
       */
 
   // the async keyword means we can make API requests
-  const form = document.querySelector(".main_form"); // get your main form so you can do JS with it
-  const submit = document.querySelector("#get-resto"); // get a reference to your submit button
-  const loadAnimation = document.querySelector(".lds-ellipsis");
-  submit.style.display = "none"; // let your submit button disappear
+  const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
+  const submit = document.querySelector('#get-resto'); // get a reference to your submit button
+  const loadAnimation = document.querySelector('.lds-ellipsis');
+  submit.style.display = 'none'; // let your submit button disappear
 
   /*
         Let's get some data from the API - it will take a second or two to load
         This next line goes to the request for 'GET' in the file at /server/routes/foodServiceRoutes.js
         It's at about line 27 - go have a look and see what we're retrieving and sending back.
        */
-  const results = await fetch("/api/foodServicePG");
+  const results = await fetch('/api/foodServicePG');
   const arrayFromJson = await results.json(); // here is where we get the data from our request as JSON
 
   /*
@@ -121,21 +121,21 @@ async function mainEvent() {
   // This IF statement ensures we can't do anything if we don't have information yet
   if (arrayFromJson.data?.length > 0) {
     // the question mark in this means "if this is set at all"
-    submit.style.display = "block"; // let's turn the submit button back on by setting it to display as a block when we have data available
+    submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
 
-    loadAnimation.classList.remove("lds-ellipsis");
-    loadAnimation.classList.add("lds-ellipsis_hidden");
+    loadAnimation.classList.remove('lds-ellipsis');
+    loadAnimation.classList.add('lds-ellipsis_hidden');
     // And here's an eventListener! It's listening for a "submit" button specifically being clicked
     // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
     let currentList = [];
 
-    form.addEventListener("input", (event) => {
+    form.addEventListener('input', (event) => {
       console.log(event.target.value);
       const filteredList = filterList(currentList, event.target.value);
       injectHTML(filteredList);
     });
 
-    form.addEventListener("submit", (submitEvent) => {
+    form.addEventListener('submit', (submitEvent) => {
       // This is needed to stop our page from changing to a new URL even though it heard a GET request
       submitEvent.preventDefault();
 
@@ -157,4 +157,4 @@ async function mainEvent() {
       It's calling the 'mainEvent' function at line 57
       It runs first because the listener is set to when your HTML content has loaded
     */
-document.addEventListener("DOMContentLoaded", async () => mainEvent()); // the async keyword means we can make API requests
+document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
