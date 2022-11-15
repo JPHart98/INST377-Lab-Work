f433a59787e3e4bbce8f1a949c2f173e7a13d593
JPHart98 <89784428+JPHart98@users.noreply.github.com>
2022-11-15 12:34:31 -0500

    Created initMap function and started markerPlace function

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -79,6 +79,20 @@ function filterList(list, filterInputValue) {
   });
 }
 
+function initMap() {
+  console.log('initMap');
+  const map = L.map('map').setView([38.9897, -76.9378], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+  return map;
+}
+
+function markerPlace(array, map) {
+  console.log('markerPlace', array);
+  const marker = L.marker([51.5, -0.09]).addTo(map);
+}
 async function mainEvent() {
   /*
         ## Main Event
@@ -86,7 +100,7 @@ async function mainEvent() {
           When you're not working in a heavily-commented "learning" file, this also is more legible
           If you separate your work, when one piece is complete, you can save it and trust it
       */
-
+  const pageMap = initMap();
   // the async keyword means we can make API requests
   const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
   const submit = document.querySelector('#get-resto'); // get a reference to your submit button
